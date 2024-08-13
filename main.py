8b98ceda6a451d129843f51a5f77661daefcc005
prmorais <prmorais1302@gmail.com>
2024-08-12 22:31:16 -0300

    Add Dialog Box

--- main.py
+++ main.py
@@ -1,30 +1,99 @@
 import flet as ft
+import ctypes
 
 
 def main(page: ft.Page):
     page.title = 'Ponto SMS'
-    page.horizontal_alignment = ft.MainAxisAlignment.CENTER
 
-    cpf = ft.TextField(
+    # Centralizando o conteúdo da janela
+    page.horizontal_alignment = ft.CrossAxisAlignment.CENTER
+    page.vertical_alignment = ft.MainAxisAlignment.CENTER
+
+    # Definindo o tamanho da janela
+    page.window_width = 800
+    page.window_height = 600
+
+    # Obtendo a resolução da tela
+    user32 = ctypes.windll.user32
+    screen_width = user32.GetSystemMetrics(0)
+    screen_height = user32.GetSystemMetrics(1)
+
+    # Calculando a posição para centralizar a janela
+    page.window_left = (screen_width - page.window_width) // 2
+    page.window_top = (screen_height - page.window_height) // 2
+
+    def validate_form(e):
+        cpf = cpf_field.value
+        start_date = start_date_field.value
+        end_date = end_date_field.value
+
+        errors = []
+
+        if not cpf:
+            errors.append('CPF é obrigatório')
+            cpf_field.error_text = 'Digite um CPF válido'
+        else:
+            cpf_field.error_text = None
+
+        if not start_date:
+            errors.append('Data inicial é obrigatória')
+            start_date_field.error_text = 'Informe a data inicial'
+        else:
+            start_date_field.error_text = None
+
+        if not end_date:
+            errors.append('Data inicial é obrigatória')
+            end_date_field.error_text = 'Informe a data inicial'
+        else:
+            end_date_field.error_text = None
+
+        cpf_field.update()
+        start_date_field.update()
+        end_date_field.update()
+
+        if not errors:
+            dialog = ft.AlertDialog(
+                title=ft.Row([
+                    ft.Icon(name=ft.icons.WARNING, color=ft.colors.RED, size=30),
+                    ft.Text('ATENÇÃO!', weight=ft.FontWeight.BOLD, size=24)
+                ], alignment=ft.MainAxisAlignment.START),
+
+                content=ft.Text('Dados enviados'),
+                actions=[
+                    ft.TextButton('Fechar', on_click=lambda e_: close_dialog(dialog))
+                ],
+                bgcolor=ft.colors.YELLOW_200,
+                shape=ft.RoundedRectangleBorder(radius=10),
+            )
+
+            # Abrindo o diálogo
+            page.dialog = dialog
+            page.dialog.open = True
+            page.update()
+
+        def close_dialog(dialog_):
+            dialog.open = False
+            page.update()
+
+    cpf_field = ft.TextField(
         label='CPF',
         col={'md': 12},
         hint_text='Digite um CPF',
         expand=True
     )
 
-    start_date = ft.TextField(
-        label='Data inicial',
+    start_date_field = ft.DatePicker(
         col={'md': 6},
         expand=True
     )
 
-    end_date = ft.TextField(
-        label='Data final',
+    end_date_field = ft.DatePicker(
         col={'md': 6},
         expand=True
     )
 
     generate_button = ft.ElevatedButton(
+        on_click=validate_form,
         text='Gerar arquivo',
         col={'md': 4},
         expand=True,
@@ -56,11 +125,11 @@ def main(page: ft.Page):
                     ft.ListTile(
                         title=ft.Text('Consulta ponto SMS')
                     ),
-                    ft.Row([
-                        cpf
+                    ft.ResponsiveRow([
+                        cpf_field
                     ]),
                     ft.ResponsiveRow([
-                        start_date, end_date
+                        start_date_field, end_date_field
                     ]),
                     ft.ResponsiveRow([
                         generate_button, cancel_button, exit_button
@@ -70,42 +139,6 @@ def main(page: ft.Page):
 
         )
     )
-
-    # def btn_generate_clicked(e):
-    #     if not cpf.value:
-    #         cpf.error_text = 'Informe o CPF'
-    #         cpf.update()
-    #     else:
-    #         cpf.clean()
-    #
-    # cpf = ft.TextField(
-    #     label='CPF',
-    #     hint_text='Digite um CPF',
-    #     width=600)
-    #
-    # start_date = ft.TextField(
-    #     label='Data inicial',
-    #     width=300)
-    #
-    # end_date = ft.TextField(
-    #     label='Data final',
-    #     width=300)
-    #
-    # generate_button = ft.ElevatedButton(
-    #     text='Gerar arquivo',
-    #     expand=True,
-    #     color='white',
-    #     bgcolor='blue',
-    #     on_click=btn_generate_clicked
-    # )
-    # cancel_button = ft.ElevatedButton(text='Cancelar', expand=True)
-    # exit_button = ft.ElevatedButton(text='Sair', expand=True)
-    #
-    # row_1 = ft.Row(controls=[cpf])
-    # row_2 = ft.Row(controls=[start_date, end_date])
-    # row_3 = ft.Row(controls=[generate_button, cancel_button, exit_button])
-    #
-    # page.add(row_1, row_2, row_3)
 
 
 ft.app(main)
