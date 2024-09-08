776daed7ace9ee30f15c9ce6b6194b48f4c34cd9
prmorais <prmorais1302@gmail.com>
2024-09-08 15:56:15 -0300

    Implementing splash screen

--- main.py
+++ main.py
@@ -1,21 +1,8 @@
 import flet as ft
 
-
-# Importações dos módulos locais
-from controls.buttons.elevated_button import (
-    generate_button,
-    cancel_button,
-    exit_button,
-    open_folder_button
-)
-from controls.inputs.input_text import (
-    cpf_field,
-    start_date_field,
-    end_date_field,
-)
-from controls.inputs.checkboxs import checkbox_excel_field, checkbox_pdf_field
-from utils.share_model import window_event, on_key_enter_event
 from models.page_manager import PageManager
+from models.splash_screen import SplashScreen
+from utils.share_model import window_event, on_key_enter_event
 
 
 def main(page: ft.Page):
@@ -24,15 +11,12 @@ def main(page: ft.Page):
     PageManager.set_page(page)
 
     # Define o nome que será exibido na barra de ferramentas da página
-    page.title = 'Ponto SMS'
-
-    # Definindo o tamanho da janela
-    page.window.width = 850
-    page.window.height = 650
+    page.title = 'Consulta ponto Eletrônico'
 
     # Centralizando o conteúdo da janela
     page.horizontal_alignment = ft.CrossAxisAlignment.CENTER
     page.vertical_alignment = ft.MainAxisAlignment.CENTER
+    page.window.center()
 
     # Define que o tema da página ser DARK (escuro)
     page.theme_mode = ft.ThemeMode.DARK
@@ -41,69 +25,21 @@ def main(page: ft.Page):
     page.window.resizable = False
     page.window.maximizable = False
     page.window.minimized = False
-    page.window.center()
 
     # Mantém a janela do aplicativo sobre as demais janelas abertas no PC
     page.window.always_on_top = True
 
-    # Intercepta o evento disparado quando o botão (X) da janela é clicado
+    # Intercepta o evento disparado quando o botão (X)
+    # da barra de título da janela é clicado
     page.window.prevent_close = True
     page.window.on_event = window_event
 
     # Intercepta o evento disparado quando a tecla (Enter) é pressionada
     page.on_keyboard_event = on_key_enter_event
 
-    # Adiciona à página um controle (Card) com os demais controles do formulário
-    page.add(
-        ft.Card(
-            content=ft.Container(
-                width=600,
-                padding=20,
-                bgcolor='#1e1f22',
-                border_radius=10,
-                border=ft.border.all(1, '#5a90fc'),
-                content=ft.Column(
-                    spacing=20,
-                    controls=[
-                        ft.ListTile(
-                            title=ft.Text(
-                                value='Consulta ponto SMS',
-                                size=25,
-                                weight=ft.FontWeight.W_500
-                            )
-                        ),
-                        ft.ResponsiveRow([
-                            cpf_field
-                        ]),
-                        ft.Container(
-                            ft.ResponsiveRow([
-                                start_date_field,
-                                end_date_field
-                            ]),
-                            margin=ft.margin.only(top=20)
-                        ),
-                        ft.Container(
-                            ft.Row([
-                                checkbox_excel_field,
-                                checkbox_pdf_field,
-                            ], alignment=ft.MainAxisAlignment.SPACE_BETWEEN
-                            ),
-                        ),
-                        ft.ResponsiveRow([
-                                generate_button,
-                        ]),
-                        ft.Container(
-                            ft.ResponsiveRow([
-                                open_folder_button,
-                                cancel_button,
-                                exit_button
-                            ]),
-                        ),
-                    ]
-                ),
-            )
-        ),
-    )
+    # Chama a função (SplashScreen). Ela exibe uma tela inicial por 3
+    # segundos e após esse tempo, exibe a janela principal da aplicação
+    SplashScreen(page=page, duration=2).show()
 
 
 ft.app(target=main)
---
+++ models/splash_screen.py
@@ -0,0 +1,167 @@
+import threading
+from time import sleep
+
+import flet as ft
+
+# Importações dos módulos locais
+from controls.buttons.elevated_button import (
+    generate_button,
+    cancel_button,
+    exit_button,
+    open_folder_button
+)
+from controls.inputs.checkboxs import checkbox_excel_field, checkbox_pdf_field
+from controls.inputs.input_text import (
+    cpf_field,
+    start_date_field,
+    end_date_field,
+)
+
+
+# Simula o efeito de fade in ou fade out
+def fade_effect(
+        container: ft.Column,
+        page: ft.Page,
+        start_opacity: int,
+        end_opacity: int,
+        step: int
+):
+    # Usa um loop aplicando o efeito fade. Dependendo dos valores passados
+    #  nos parâmetros, o efeito pode ser (fade in) ou (fade out)
+    for opacity in range(start_opacity, end_opacity, step):
+        container.opacity = opacity / 100
+        page.update()
+        sleep(0.05)
+
+
+class SplashScreen:
+    def __init__(self, page: ft.Page, duration: int = 3):
+        self.splash_controls = None
+        self.page = page
+        self.duration = duration
+
+    # Cria e exibe uma tela inicial (splash show) antes de exibir
+    # a tela principal do sistema
+    def show(self):
+        # Esconde a barra de títulos da janela
+        self.page.window.title_bar_hidden = True
+
+        # Definindo o tamanho da janela
+        self.page.window.width = 600
+        self.page.window.height = 400
+
+        # Conteúdo fixo do splash screen
+        title = 'Consulta Ponto Eletrônico'
+        image_src = 'assets/logo_pgm.png'
+        text = 'Departamento de Tecnologia da Informação'
+
+        # Criando os controles da tela inicial (splash screen)
+        self.splash_controls = [
+            ft.Row(controls=[
+                ft.Text(value=title, size=30)
+            ],
+                alignment=ft.MainAxisAlignment.CENTER),
+
+            ft.Row(controls=[
+                ft.Image(src=image_src, width=500),
+            ], alignment=ft.MainAxisAlignment.CENTER),
+
+            ft.Row(controls=[
+                ft.Text(value=text, size=20),
+            ], alignment=ft.MainAxisAlignment.CENTER)
+        ]
+
+        # Adicionando os controles à página
+        self.page.controls.append(
+            ft.Column(
+                controls=self.splash_controls,
+            )
+        )
+        # Atualiza a página
+        self.page.update()
+
+        # Timer para esconder o splash screen após a duração especificada
+        threading.Timer(self.duration, self.hide).start()
+
+    # Retira a tela inicial (splash show) da janela,
+    # cria e exibe a tela principal do sistema
+    def hide(self):
+        # Exibe a barra de título da janela
+        self.page.window.title_bar_hidden = False
+
+        # Definindo o tamanho da janela
+        self.page.window.width = 850
+        self.page.window.height = 650
+
+        # Centralizando o conteúdo da janela
+        self.page.window.center()
+
+        # Limpar a tela e trocar para a tela principal
+        self.page.controls.clear()
+
+        # Atribui a variável (main_screen) os controles da página principal
+        main_screen = ft.Column([
+            ft.Card(
+                content=ft.Container(
+                    width=600,
+                    padding=20,
+                    bgcolor='#1e1f22',
+                    border_radius=10,
+                    border=ft.border.all(1, '#5a90fc'),
+                    content=ft.Column(
+                        spacing=20,
+                        controls=[
+                            ft.ListTile(
+                                title=ft.Text(
+                                    value='Dados da consulta',
+                                    size=25,
+                                    weight=ft.FontWeight.W_500
+                                )
+                            ),
+                            ft.ResponsiveRow([
+                                cpf_field
+                            ]),
+                            ft.Container(
+                                ft.ResponsiveRow([
+                                    start_date_field,
+                                    end_date_field
+                                ]),
+                                margin=ft.margin.only(top=20)
+                            ),
+                            ft.Container(
+                                ft.Row([
+                                    checkbox_excel_field,
+                                    checkbox_pdf_field,
+                                ], alignment=ft.MainAxisAlignment.SPACE_BETWEEN
+                                ),
+                            ),
+                            ft.ResponsiveRow([
+                                generate_button,
+                            ]),
+                            ft.Container(
+                                ft.ResponsiveRow([
+                                    open_folder_button,
+                                    cancel_button,
+                                    exit_button
+                                ]),
+                            ),
+                        ]
+                    ),
+                )
+            ),
+        ])
+
+        # Aplica um efeito de fade in nos controles da página principal
+        fade_effect(
+            container=main_screen,
+            page=self.page,
+            start_opacity=0,
+            end_opacity=101,
+            step=20
+
+        )
+
+        # Adiciona os controles à pagina principal e atualiza a página
+        self.page.controls.append(main_screen)
+        self.page.update()
+
